0415bf0a7280375a034c1e92e10987ba437ddbb7
AyoJunji <joseramos@knights.ucf.edu>
2022-08-23 19:03:14 -0400

    Initial Commit!

---
+++ Zork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Zork!");
+        }
+    }
+}
