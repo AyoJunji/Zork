06377fbea5e896548c881743dbec5e12affe9218
AyoJunji <joseramos@knights.ucf.edu>
2022-08-25 14:22:21 -0400

    Zork 1.3 Submodule
    The Game Loop

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,11 +8,48 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.QUIT:
+                        outputString = "Thank you for playing!\nPress any key to continue . . .";
+                        break;
+
+                    case Commands.NORTH:
+                        outputString = "You moved NORTH.";
+                        break;
+
+                    case Commands.SOUTH:
+                        outputString = "You moved SOUTH.";
+                        break;
+
+                    case Commands.WEST:
+                        outputString = "You moved WEST.";
+                        break;
+
+                    case Commands.EAST:
+                        outputString = "You moved EAST.";
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = ("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        break;
+
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
 
-        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+        private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
     }
 }
