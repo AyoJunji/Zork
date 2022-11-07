7f98e112648dac65ca689e65917e6870b8e90efd
Junji <joseramos@knights.ucf.edu>
2022-11-07 14:40:26 -0500

    Assignment 9
    Take / Drop

---
+++ Zork.Common/Player.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zork.Common
+{
+    public class Player
+    {        
+        public Room CurrentRoom
+        {
+            get => _currentRoom;
+            set => _currentRoom = value;
+        }
+
+        public List<Item> Inventory { get; }
+
+        public Player(World world, string startingLocation)
+        {
+            _world = world;
+
+            if (_world.RoomsByName.TryGetValue(startingLocation, out _currentRoom) == false)
+            {
+                throw new Exception($"Invalid starting location: {startingLocation}");
+            }
+
+            Inventory = new List<Item>();
+        }
+
+        public bool Move(Directions direction)
+        {
+            bool didMove = _currentRoom.Neighbors.TryGetValue(direction, out Room neighbor);
+            if (didMove)
+            {
+                CurrentRoom = neighbor;
+            }
+
+            return didMove;
+        }
+
+        public void AddToInventory(Item itemToTake)
+        {
+            Inventory.Add(itemToTake);
+        }
+
+        public void RemoveFromInventory(Item itemToRemove)
+        {
+            Inventory.Remove(itemToRemove); 
+        }
+
+        private World _world;
+        private Room _currentRoom;
+    }
+}
